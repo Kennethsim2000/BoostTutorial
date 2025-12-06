1a98959d6a9b095ab87b30175fe126d7d00e287d
kenneth <kenneth12345645@gmail.com>
2025-12-06 11:28:58 +0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <cstdint>
+#include <string>
+#include <iostream>
+
+using OrderId = uint64_t;
+using ClientId = std::string;
+
+enum class Side
+{
+    Buy,
+    Sell
+};
+
+struct Order
+{
+    OrderId id;
+    ClientId client;
+    Side side;
+    double price;
+    uint64_t qty;
+    uint64_t original_qty;
+    std::chrono::system_clock::time_point ts;
+};
+
+struct Trade
+{
+    OrderId buy_order;
+    OrderId sell_order;
+    double price;
+    uint64_t qty;
+    std::chrono::system_clock::time_point ts;
+};
+
+int main()
+{
+    std::cout << "Initial commit" << std::endl;
+    return 0;
+}
