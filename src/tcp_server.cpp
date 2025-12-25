8079b9fac343fdae71c74249311a3fc8a20c4317
kenneth <kenneth12345645@gmail.com>
2025-12-25 22:02:08 +0800

    Implement on_read and do_read

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -44,104 +44,136 @@ namespace net
                 do_accept();
             });
     }
-}
 
-// ======================================================================
-// ============================= SESSION =================================
-// ======================================================================
+    // ======================================================================
+    // ============================= SESSION =================================
+    // ======================================================================
 
-TCPServer::Session::Session(tcp::socket socket, OrderBook &book)
-    : socket_(std::move(socket)),
-      book_(book)
-{
-}
+    TCPServer::Session::Session(tcp::socket socket, OrderBook &book)
+        : socket_(std::move(socket)),
+          book_(book)
+    {
+    }
 
-// ------------------------------------------------------------
-// Session entry point
-// ------------------------------------------------------------
-void TCPServer::Session::start()
-{
-    // TODO:
-    // Call do_read();
-    std::cout << "Client connected from: "
-              << socket->remote_endpoint().address().to_string()
-              << ":" << socket->remote_endpoint().port() << std::endl;
-}
+    // ------------------------------------------------------------
+    // Session entry point
+    // ------------------------------------------------------------
+    void TCPServer::Session::start()
+    {
+        do_read();
+        std::cout << "Client connected from: "
+                  << socket_.remote_endpoint().address().to_string()
+                  << ":" << socket_.remote_endpoint().port() << std::endl;
+    }
 
-// ------------------------------------------------------------
-// Set up async read until newline
-// ------------------------------------------------------------
-void TCPServer::Session::do_read()
-{
-    // TODO:
-    // async_read_until(socket_, buffer_, '\n', callback)
-    //
-    // callback calls on_read(...)
-}
+    // ------------------------------------------------------------
+    // Set up async read until newline
+    // ------------------------------------------------------------
+    void TCPServer::Session::do_read()
+    {
+        // TODO:
+        // async_read_until(socket_, buffer_, '\n', callback)
+        //
+        // callback calls on_read(...)
+        // read until new line
+        // Capturing self ensures the Session stays alive until handler completes
+        async_read_until(socket_, buffer_, "\n",
+                         [this, self = shared_from_this()](boost::system::error_code ec, size_t n)
+                         {
+                             on_read(ec, n);
+                         });
+    }
 
-// ------------------------------------------------------------
-// Handle a full incoming line
-// ------------------------------------------------------------
-void TCPServer::Session::on_read(error_code ec,
-                                 std::size_t bytes_transferred)
-{
-    // TODO:
-    // if (ec) handle disconnect or error
-    // else:
-    //   extract line from buffer_:
-    //     std::istream is(&buffer_);
-    //     std::string line;
-    //     std::getline(is, line);
-    //
-    //   process_line(line);
-    //
-    //   continue reading:
-    //     do_read();
-}
+    // ------------------------------------------------------------
+    // Handle a full incoming line
+    // ------------------------------------------------------------
+    void TCPServer::Session::on_read(boost::system::error_code ec,
+                                     std::size_t bytes_transferred)
+    {
+        if (ec)
+        {
+            if (ec == boost::asio::error::eof)
+            {
+                if (socket_.is_open())
+                    socket_.close();
+            }
+            else
+            {
+                std::cerr << "Read error: " << ec.message() << std::endl;
+            }
+            return;
+        }
+        std::istream is(&buffer_);
+        std::string line;
+        std::getline(is, line);
+        process_line(line);
+        do_read();
+    }
 
-// ------------------------------------------------------------
-// Parse commands and call OrderBook
-// ------------------------------------------------------------
-void TCPServer::Session::process_line(const std::string &line)
-{
-    // HINTS:
-    // 1. Tokenize using std::istringstream iss(line)
-    // 2. Read first word → command
-    // 3. if ORDER:
-    //       parse "side price qty client"
-    //       build Order ord;
-    //       auto trades = book_.place_order(...)
-    //       format a response string summarizing trades
-    //       write_response(resp);
-    //
-    //    if CANCEL:
-    //       parse id
-    //       bool ok = book_.cancel_order(id)
-    //       write_response(ok?"CANCELLED\n":"NOT_FOUND\n");
-    //
-    //    if SNAPSHOT:
-    //       parse depth
-    //       auto json = book_.snapshot_top(depth)
-    //       write_response(json + "\n");
-    //
-    //    else:
-    //       write_response("ERROR unknown\n");
-}
+    // ------------------------------------------------------------
+    // Parse commands and call OrderBook
+    // ------------------------------------------------------------
+    void TCPServer::Session::process_line(const std::string &line)
+    {
+        std::vector<std::string> tokens;
+        std::istringstream iss(line);
+        std::string token;
+        while (iss >> token)
+        {
+            tokens.push_back(token);
+        }
+        std::string command = tokens.at(0);
+        if (command == "ORDER")
+        {
+        }
+        else if (command == "CANCEL")
+        {
+        }
+        else if (command == "SNAPSHOT")
+        {
+        }
+        else
+        {
+            write_response("ERROR unknwon command\n");
+        }
+        // HINTS:
+        // 1. Tokenize using std::istringstream iss(line)
+        // 2. Read first word → command
+        // 3. if ORDER:
+        //       parse "side price qty client"
+        //       build Order ord;
+        //       auto trades = book_.place_order(...)
+        //       format a response string summarizing trades
+        //       write_response(resp);
+        //
+        //    if CANCEL:
+        //       parse id
+        //       bool ok = book_.cancel_order(id)
+        //       write_response(ok?"CANCELLED\n":"NOT_FOUND\n");
+        //
+        //    if SNAPSHOT:
+        //       parse depth
+        //       auto json = book_.snapshot_top(depth)
+        //       write_response(json + "\n");
+        //
+        //    else:
+        //       write_response("ERROR unknown\n");
+    }
 
-// ------------------------------------------------------------
-// Send response back to client
-// ------------------------------------------------------------
-void TCPServer::Session::write_response(const std::string &resp)
-{
-    // TODO:
-    // Use async_write(socket_, buffer, callback)
-    //
-    // IMPORTANT:
-    // Response must live until async_write finishes.
-    // So wrap in shared_ptr<string>:
-    //
-    // auto out = std::make_shared<std::string>(resp);
-    // async_write(socket_, buffer(*out), [self,out](...) { ... });
-}
+    // ------------------------------------------------------------
+    // Send response back to client
+    // ------------------------------------------------------------
+    void TCPServer::Session::write_response(const std::string &resp)
+    {
+        // TODO:
+        // Use async_write(socket_, buffer, callback)
+        //
+        // IMPORTANT:
+        // Response must live until async_write finishes.
+        // So wrap in shared_ptr<string>:
+        //
+        // auto out = std::make_shared<std::string>(resp);
+        // async_write(socket_, buffer(*out), [self,out](...) { ... });
+    }
 
 } // namespace net
